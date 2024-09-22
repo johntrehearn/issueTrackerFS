3aefbce37a19fddd3ce85fba96f4ce27b551d4f9
johntrehearn <johntrehearnbc@gmail.com>
2024-09-22 19:17:36 +0300

    NW servers BE

---
+++ next-app/app/components/ProductCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductCard = () => {
+    return (
+        <div>
+            <button onClick={() => console.log('Added to cart')}>Add to cart</button>
+        </div>
+    )
+}
+
+export default ProductCard
