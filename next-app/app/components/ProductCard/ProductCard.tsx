3df62b01189931ba7b30b35d343471ad144f1e93
johntrehearn <johntrehearnbc@gmail.com>
2024-09-23 12:25:52 +0300

    Tailwind styling

--- next-app/app/components/ProductCard/ProductCard.tsx
+++ next-app/app/components/ProductCard/ProductCard.tsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import AddToCart from '../AddToCart'
-import styles from './ProductCard.module.css'
+// REMOVED FOR TAILWIND EG - import styles from './ProductCard.module.css'
 
 // styles imported from ProductCard.module.css is a JS object. So the classes we define int the css module will end up being properties of the object (styles object)
 
 
+//OLD VERSION for CSS MODULES
+// const ProductCard = () => {
+//     return (
+//         <div className={styles.card}>
+//             <AddToCart />
+//         </div>
+//     )
+// }
+
+//export default ProductCard
+
+
+// With Tailwind CSS if you press CTRL and SPACE you will get a list of all the Tailwind classes you can use. But it shows on MAC automatically.
 
 const ProductCard = () => {
     return (
-        <div className={styles.card}>
+        <div className='p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-600'>
             <AddToCart />
         </div>
     )
